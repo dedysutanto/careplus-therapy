3d6d6a44ab5ed77c4e4b2c7623227555cb88aed1
gkiserpong <gkiserpongit@gmail.com>
2022-10-11 06:25:31 +0700

    Redirect all to login

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
-    re_path(r'', include(wagtail_urls)),
+    #re_path(r'', include(wagtail_urls)),
+    re_path(r'', include(wagtailadmin_urls)),
 ]
 
 
