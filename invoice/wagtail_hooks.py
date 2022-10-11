e8cf6cbc903f31957deaf0411ee6e2d7910d82a6
gkiserpong <gkiserpongit@gmail.com>
2022-10-11 17:02:06 +0700

    Rearrange Menu

--- invoice/wagtail_hooks.py
+++ invoice/wagtail_hooks.py
@@ -20,7 +20,7 @@ class InvoicesAdmin(ModelAdmin):
     base_url_path = 'invoices'  # customise the URL from default to admin/bookadmin
     menu_label = 'Invoice'  # ditch this to use verbose_name_plural from model
     menu_icon = 'success'  # change as required
-    menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
+    menu_order = 90  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
     add_to_admin_menu = True  # or False to exclude your model from the menu
--- schedule/wagtail_hooks.py
+++ schedule/wagtail_hooks.py
@@ -43,7 +43,7 @@ class SchedulesAdmin(ModelAdmin):
     base_url_path = 'schedule'  # customise the URL from default to admin/bookadmin
     menu_label = 'Jadwal'  # ditch this to use verbose_name_plural from model
     menu_icon = 'date'  # change as required
-    menu_order = 90  # will put in 3rd place (000 being 1st, 100 2nd)
+    menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
     add_to_admin_menu = True  # or False to exclude your model from the menu
